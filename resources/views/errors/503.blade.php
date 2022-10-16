4af52aa53312e5b49f47989fa732fa9da32a7525
Fortunato Estorgio <contact@estorgio.com>
2022-10-16 10:16:56 +0800

    Add 503 Service Unavailable page

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,12 @@
+<x-layout>
+    <x-slot:title>503 Service Unavailable</x-slot>
+
+        <div class="container">
+            <div class="row">
+                <div class="col text-center http-error">
+                    <h1>503</h1>
+                    <p>The site is currently under maintenance. We will be back soon.</p>
+                </div>
+            </div>
+        </div>
+</x-layout>

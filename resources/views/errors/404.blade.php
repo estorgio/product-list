56431d0b2cebfe9bee188cc30a8d081340823eca
Fortunato Estorgio <contact@estorgio.com>
2022-10-16 10:16:24 +0800

    Add 404 Not Found page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,12 @@
+<x-layout>
+    <x-slot:title>404 Page Not Found</x-slot>
+
+        <div class="container">
+            <div class="row">
+                <div class="col text-center http-error">
+                    <h1>404</h1>
+                    <p>The page you are looking for doesn't exist.</p>
+                </div>
+            </div>
+        </div>
+</x-layout>
